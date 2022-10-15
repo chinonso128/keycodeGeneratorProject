790e0111df72169e16b69e125609448022c0aa18
Chinonso Peace Mbama <mbamachinonso@gmail.com>
2022-10-15 19:59:50 +0100

    more changes

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 const KeyDisplay = document.querySelector('.key h2');
-const keycodeDisplay = document.querySelector('keycode h2');
-const keycodeD =document.querySelector('keycode');
+const keycodeDisplay = document.querySelector('.keycode h2');
+const keycodeD =document.querySelector('.keycode');
 const welcometext = document.querySelector('.welcomeText');
 
 window.addEventListener('keydown', (e) =>{
     welcometext.classList.add('hide')
     KeyDisplay.innerText = e.key;
-    keycodeDisplay.innerText = e.keycode;
-    if(e.keycode === 32){
+    keycodeDisplay.innerText = e.keyCode;
+    if(e.keyCode === 32){
         KeyDisplay.innerText = `'space'`;
     }
-})
+});
 
-keycodeD.addEventListener('click' (e)=>{
-    const textArea = document.createElement('textarea' )
+keycodeD.addEventListener('click', (e) =>{
+    const textArea = document.createElement('textarea');
+    textArea.setAttribute('readonly', '');
+    textArea.style.position= 'absolute';
+    textArea.value = keycodeD.querySelector('h2').innerText;
+    document.body.appendChild(textArea)
+    textArea.select();
+    document.execCommand('copy');
+    Document.body.removeChild(textArea);
+    keycodeD.querySelector('p').innerText = 'copied';
 })
